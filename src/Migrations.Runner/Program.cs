6e0e5dc21f511d838cc9199365271f3b418d2dd5
nikolaia <nikolaia@users.noreply.github.com>
2017-11-29 15:37:08 +0100

    Add migrationrunner example for future reference

---
+++ src/Migrations.Runner/Program.cs
@@ -0,0 +1,29 @@
+﻿using CLAP;
+
+namespace MyAppMigrations.Runner
+{
+    class Program
+    {
+
+        [Verb(IsDefault = true)]
+        static void Migrate(
+            [Required] string connectionString,
+            [Required] string clientId,
+            [Required] string certThumbPrint,
+            [Required] string authority)
+        {
+            Runner.MigrateToLatest(connectionString, clientId, certThumbPrint, authority);
+        }
+
+        [Help]
+        [Empty]
+        static void ShowHelp(string help)
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Run<Program>(args);
+        }
+    }
+}
---
+++ src/Migrations.Runner/Runner.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+
+namespace MyAppMigrations.Runner
+{
+    internal static class Runner
+    {
+        public class MigrationOptions : IMigrationProcessorOptions
+        {
+            public bool PreviewOnly { get; set; }
+            public string ProviderSwitches { get; set; }
+            public int Timeout { get; set; }
+        }
+
+        public static void MigrateToLatest(string connectionString, string clientId, string certThumbPrint, string authority)
+        {
+            var announcer = new TextWriterAnnouncer(s => { 
+                System.Console.WriteLine(s);
+                System.Diagnostics.Debug.WriteLine(s);
+            });
+            var assembly = Assembly.Load("MyAppMigrations");
+
+            var migrationContext = new RunnerContext(announcer)
+            {
+                Namespace = "MyAppMigrations"
+            };
+
+            var options = new MigrationOptions { PreviewOnly = false, Timeout = 60 };
+            var factory = new AzureSqlProcessorFactory(clientId, certThumbPrint, authority);
+
+            using (var processor = factory.Create(connectionString, announcer, options))
+            {
+                var runner = new MigrationRunner(assembly, migrationContext, processor);
+                runner.ListMigrations();
+                runner.MigrateUp(true);
+            }
+        }
+    }
+}
