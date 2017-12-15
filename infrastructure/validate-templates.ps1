91b1f725de52e5ec65a974917c08dffc42bd499e
nikolaia <nikolaia@users.noreply.github.com>
2017-12-15 14:47:19 +0100

    Add example of arm templates replacing the old infrastructure folder

---
+++ infrastructure/validate-templates.ps1
@@ -0,0 +1,5 @@
+az account set -s "Test"
+az group deployment validate -g MyApp.Dev --template-file "arm/azuredeploy.json" --parameters @arm/parameters/dev.parameters.json
+az group deployment validate -g MyApp.Test --template-file "arm/azuredeploy.json" --parameters @arm/parameters/test.parameters.json
+az account set -s "Production"
+az group deployment validate -g MyApp --template-file "arm/azuredeploy.json" --parameters @arm/parameters/production.parameters.json
