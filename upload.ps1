c6847b3e1227605b719d0e77ac708dae85d3641d
nikolaia <nikolaia@users.noreply.github.com>
2018-02-20 09:07:48 +0100

    Simplify upload.ps1 artifactdir assignment

--- upload.ps1
+++ upload.ps1
@@ -10,15 +10,13 @@ $apiUrl = "https://$appName.scm.azurewebsites.net/api/zipdeploy"
 $vstsWorkDir = $env:SYSTEM_DEFAULTWORKINGDIRECTORY
 $vstsReleaseDefName = $env:RELEASE_DEFINITIONNAME
 
-if ($vstsWorkDir -and $vstsReleaseDefName) {
-    $artifactDir = "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)/$($env:RELEASE_DEFINITIONNAME)/drop"
-    $zip = Get-ChildItem -Path $artifactDir -Filter *.zip | Select-Object -First 1
-    $zipPath = "$artifactDir/$($zip.Name)"
-} else {
-    $artifactDir = split-path -parent $MyInvocation.MyCommand.Definition
-    $zip = Get-ChildItem -Path $artifactDir -Filter *.zip | Select-Object -First 1
-    $zipPath = "$artifactDir/$($zip.Name)"
-}
+$artifactDir = if ($vstsWorkDir -and $vstsReleaseDefName) { 
+        "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)/$($env:RELEASE_DEFINITIONNAME)/drop"
+    } else { 
+        split-path -parent $MyInvocation.MyCommand.Definition
+    }
 
+$zip = Get-ChildItem -Path $artifactDir -Filter *.zip | Select-Object -First 1
+$zipPath = "$artifactDir/$($zip.Name)"
 Write-Host "Calling $apiUrl with $zipPath using token '$bearerToken'"
 Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=$bearerToken} -Method POST -InFile $zipPath -ContentType "multipart/form-data"
